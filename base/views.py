8ca85e46a5d1ca0137eaa3afeb6364cb0691fac9
Mar <margaritis1argyrou@gmail.com>
2022-05-24 14:09:37 +0100

    Link to posts working with passing an object

--- base/views.py
+++ base/views.py
@@ -5,6 +5,11 @@ def home(request):
     context = 'context message here'
     return render(request, 'base/home.html', {'context': context})
 
-def room(request):
-    return HttpResponse('room')
+def post(request):
+    posts = [
+        {'id': 1, 'user': 'Jordan - posted 3 hours ago', 'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
+        {'id': 2, 'user': 'Jenny - posted 5 hours ago', 'text': 'Lorem ipsum, consectetur adipisicing elit.'},
+        {'id': 3, 'user': 'Mario - posted 6 hours ago', 'text': 'Lorem ipsum, consectetur adipisicing elit. consectetur adipisicing.'},
+    ]
+    return render(request, 'base/post.html', {'posts': posts})
 
