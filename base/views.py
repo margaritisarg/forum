9e585834ae47f07c3c701ad8a51aa813e1c01dbf
Mar <margaritis1argyrou@gmail.com>
2022-05-25 13:29:44 +0100

    DB setup n migratration. Post classs n link to app

--- base/views.py
+++ base/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 def home(request):
-    context = 'context message here'
-    return render(request, 'base/home.html', {'context': context})
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'base/home.html', context)
 
 def post(request):
-    posts = [
-        {'id': 1, 'user': 'Jordan - posted 3 hours ago', 'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'},
-        {'id': 2, 'user': 'Jenny - posted 5 hours ago', 'text': 'Lorem ipsum, consectetur adipisicing elit.'},
-        {'id': 3, 'user': 'Mario - posted 6 hours ago', 'text': 'Lorem ipsum, consectetur adipisicing elit. consectetur adipisicing.'},
-    ]
-    return render(request, 'base/post.html', {'posts': posts})
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'base/post.html', context)
+
+
+
 
