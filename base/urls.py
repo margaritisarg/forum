54a5609267e1f77a0b45dd331290e19ef276f617
Mar <margaritis1argyrou@gmail.com>
2022-05-27 13:16:20 +0100

    User can log in and out now

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
     path('post/', views.post, name='post')
 ]
